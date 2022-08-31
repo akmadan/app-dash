e36b1a25bfe58e1c1a629533095227b80dc9a9f0
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2022-08-31 12:31:51 +0530

    Update dropDownButton.dart

--- dropDownButton.dart
+++ dropDownButton.dart
@@ -13,35 +13,47 @@
   
   String? selectedValue;
 
- Container(
-                width: 200,
-                child: DropdownButton2(
-                  hint: Text(
-                    'Select Item',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Theme.of(context).hintColor,
-                    ),
-                  ),
-                  items: items
-                      .map((item) => DropdownMenuItem<String>(
-                            value: item,
-                            child: Text(
-                              item,
-                              style: const TextStyle(
-                                fontSize: 14,
+  Container(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(16, 12, 16, 0),
+                            width: double.infinity,
+                            height: 70,
+                            child: DropdownButtonHideUnderline(
+                              child: DropdownButton2(
+                                buttonPadding: EdgeInsets.all(8),
+                                buttonDecoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Color(0xFFE7EAEB),
+                                      width: 1,
+                                    ),
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Colors.white),
+                                hint: Text(
+                                  'Select Item',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Theme.of(context).hintColor,
+                                  ),
+                                ),
+                                items: ['NSSF Hi-Innovator', 'Other']
+                                    .map((item) => DropdownMenuItem<String>(
+                                          value: item,
+                                          child: Text(
+                                            item,
+                                            style: const TextStyle(
+                                              fontSize: 14,
+                                            ),
+                                          ),
+                                        ))
+                                    .toList(),
+                                value: partnerProgram,
+                                onChanged: (value) {
+                                  setState(() {
+                                    partnerProgram = value as String;
+                                  });
+                                },
+                                buttonHeight: 40,
+                                buttonWidth: 140,
+                                itemHeight: 40,
                               ),
-                            ),
-                          ))
-                      .toList(),
-                  value: selectedValue,
-                  onChanged: (value) {
-                    setState(() {
-                      selectedValue = value as String;
-                    });
-                  },
-                  buttonHeight: 40,
-                  buttonWidth: 140,
-                  itemHeight: 40,
-                ),
-              ),
+                            )),
