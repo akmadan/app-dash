2b5d7af07669db20fca406ac07febb2ee8c38be4
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2022-08-31 16:02:12 +0530

    Create dateTimeInTextField.dart

---
+++ dateTimeInTextField.dart
@@ -0,0 +1,30 @@
+TextField(
+                    controller: dateInput,
+                    //editing controller of this TextField
+                    decoration:
+                        AppStyles.TextFieldInputDecoration('Select Date'),
+                    readOnly: true,
+                    //set it true, so that user will not able to edit text
+                    onTap: () async {
+                      DateTime? pickedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(1950),
+                          //DateTime.now() - not to allow to choose before today.
+                          lastDate: DateTime(2100));
+
+                      if (pickedDate != null) {
+                        print(
+                            pickedDate); //pickedDate output format => 2021-03-10 00:00:00.000
+                        String formattedDate =
+                            DateFormat('yyyy-MM-dd').format(pickedDate);
+                        print(
+                            formattedDate); //formatted date output using intl package =>  2021-03-16
+                        setState(() {
+                          pickeDate = pickedDate;
+                          dateInput.text =
+                              formattedDate; //set output date to TextField value.
+                        });
+                      } else {}
+                    },
+                  ),
