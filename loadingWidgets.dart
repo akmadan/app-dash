c1eeef2023612e1debe1ac8c30101e8b9db4177b
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2022-09-04 22:57:25 +0530

    Added Shimmer

--- loadingWidgets.dart
+++ loadingWidgets.dart
@@ -5,3 +5,43 @@ Widget loadingWidget() {
     ),
   );
 }
+
+
+class ShimmerWidget extends StatelessWidget {
+  const ShimmerWidget({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+                      child: Container(
+                        margin: EdgeInsets.symmetric(vertical: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.grey,
+                              ),
+                              width: 300,
+                              height: 20,
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.grey,
+                              ),
+                              width: 200,
+                              height: 20,
+                            )
+                          ],
+                        ),
+                      ),
+                      baseColor: Colors.grey[300]!,
+                      highlightColor: Colors.grey[100]!,
+                    );
+  }
+}
