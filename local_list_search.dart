f6693c3687957d12b6410e92f10982b22299e517
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2022-09-07 02:09:24 +0530

    Create local_list_search.dart

---
+++ local_list_search.dart
@@ -0,0 +1,143 @@
+/// List Page UI with search bar
+
+class FilterLocalListPage extends StatefulWidget {
+  @override
+  FilterLocalListPageState createState() => FilterLocalListPageState();
+}
+
+class FilterLocalListPageState extends State<FilterLocalListPage> {
+  late List<Book> books;
+  String query = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    books = allBooks;
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text(MyApp.title),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            buildSearch(),
+            Expanded(
+              child: ListView.builder(
+                itemCount: books.length,
+                itemBuilder: (context, index) {
+                  final book = books[index];
+
+                  return buildBook(book);
+                },
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget buildSearch() => SearchWidget(
+        text: query,
+        hintText: 'Title or Author Name',
+        onChanged: searchBook,
+      );
+
+  Widget buildBook(Book book) => ListTile(
+        leading: Image.network(
+          book.urlImage,
+          fit: BoxFit.cover,
+          width: 50,
+          height: 50,
+        ),
+        title: Text(book.title),
+        subtitle: Text(book.author),
+      );
+
+  // main logic
+  
+  void searchBook(String query) {
+    final books = allBooks.where((book) {
+      final titleLower = book.title.toLowerCase();
+      final authorLower = book.author.toLowerCase();
+      final searchLower = query.toLowerCase();
+
+      return titleLower.contains(searchLower) ||
+          authorLower.contains(searchLower);
+    }).toList();
+
+    setState(() {
+      this.query = query;
+      this.books = books;
+    });
+  }
+}
+
+
+
+
+
+/// Search Widget 
+
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatefulWidget {
+  final String text;
+  final ValueChanged<String> onChanged;
+  final String hintText;
+
+  const SearchWidget({
+    Key? key,
+    required this.text,
+    required this.onChanged,
+    required this.hintText,
+  }) : super(key: key);
+
+  @override
+  _SearchWidgetState createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  final controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final styleActive = TextStyle(color: Colors.black);
+    final styleHint = TextStyle(color: Colors.black54);
+    final style = widget.text.isEmpty ? styleHint : styleActive;
+
+    return Container(
+      height: 42,
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+        border: Border.all(color: Colors.black26),
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          icon: Icon(Icons.search, color: style.color),
+          suffixIcon: widget.text.isNotEmpty
+              ? GestureDetector(
+                  child: Icon(Icons.close, color: style.color),
+                  onTap: () {
+                    controller.clear();
+                    widget.onChanged('');
+                    FocusScope.of(context).requestFocus(FocusNode());
+                  },
+                )
+              : null,
+          hintText: widget.hintText,
+          hintStyle: style,
+          border: InputBorder.none,
+        ),
+        style: style,
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
