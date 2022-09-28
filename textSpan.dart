73d5ca38e52a9c870e3a411df0562692109c85b4
Akshit Madan <56507451+akmadan@users.noreply.github.com>
2022-09-28 16:40:53 +0530

    Create textSpan.dart

---
+++ textSpan.dart
@@ -0,0 +1,11 @@
+Text.rich(
+            TextSpan(
+              text: 'This is textspan ',
+              children: <InlineSpan>[
+                TextSpan(
+                  text: 'Widget in flutter',
+                  style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                )
+              ]
+            )
+          ),
